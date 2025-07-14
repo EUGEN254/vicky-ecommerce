8f91b254277a65d64ffab8bfdf88f3d04bcf49f0
EUGEN254 <bitinyoeugen@gmail.com>
2025-07-14 17:04:30 +0300

    l

--- Front-end/src/assets/assets.js
+++ Front-end/src/assets/assets.js
@@ -1,38 +1,18 @@
 import logo from './logo.png'
-import background from './background.png'
 import cart_icon from './cart_icon.png'
-import bin_icon from './bin_icon.png'
-import dropdown_icon from './dropdown_icon.png'
-import exchange_icon from './exchange_icon.png'
 import profile_icon from './profile_icon.png'
 import uploadArea from './uploadArea.svg'
 import totalRevenue from './totalRevenueIcon.svg'
-import quality_icon from './quality_icon.png'
-import search_icon from './search_icon.png'
-import star_dull_icon from './star_dull_icon.png'
-import star_icon from './star_icon.png'
-import support_img from './support_img.png'
 import menu_icon from './menu_icon.png'
-import contact_img from './contact_img.png'
-import razorpay_logo from './razorpay_logo.png'
-import stripe_logo from './stripe_logo.png'
 import cross_icon from './cross_icon.png'
 import basket_icon from './basket_icon.png'
 import closeIcon from './closeIcon.svg'
 import arrowIcon from './arrowIcon.svg'
-import listIcon from "./listIcon.svg"
-import locationFilledIcon from './locationFilledIcon.svg'
 import locationIcon from './locationIcon.svg'
-import starIconFilled from './starIconFilled.svg'
 import linkendinIcon from './linkendinIcon.svg'
 import instagramIcon from './instagramIcon.svg'
 import facebookIcon from './facebookIcon.svg'
 import twitterIcon from './twitterIcon.svg'
-import freeWifiIcon from './freeWifiIcon.svg'
-import freeBreakfastIcon from './freeBreakfastIcon.svg'
-import roomServiceIcon from './roomServiceIcon.svg'
-import mountainIcon from './mountainIcon.svg'
-import poolIcon from './poolIcon.svg'
 import vanbackground from './vanbackground.png'
 import dashboardIcon from "./dashboardIcon.svg";
 import addIcon from "./addIcon.svg";
@@ -41,12 +21,9 @@ import addIcon from "./addIcon.svg";
 export const assets = {
     logo,
     uploadArea,
-    freeWifiIcon,
     dashboardIcon,
     listIcon,
-    addIcon,
     totalRevenue,
-    hero_img,
     freeBreakfastIcon,
     roomServiceIcon,
     mountainIcon,
@@ -59,59 +36,16 @@ export const assets = {
     twitterIcon,
     facebookIcon,
     linkendinIcon,
-    background,
-    starIconFilled,
-    locationFilledIcon,
     dropdown_icon,
-    exchange_icon,
     profile_icon,
-    quality_icon,
-    search_icon,
-    star_dull_icon,
-    star_icon,
     bin_icon,
-    support_img,
     menu_icon,
-    contact_img,
-    razorpay_logo,
-    stripe_logo,
-    cross_icon,
     basket_icon,
     closeIcon,
 }
 
-// Testimonials Dummy Data
-export const testimonials = [
-  { id: 1, 
-    name: "Emma Rodriguez", 
-    address: "Nairobi", 
-    image: "https://images.unsplash.com/photo-1633332755192-727a05c4013d?q=80&w=200", 
-    rating: 5, review: "I've used many shopping platforms before, but none compare to the personalized experience and attention to detail that Gracies shoe hub provides." 
-  },
-  { id: 2, 
-    name: "Liam Johnson", 
-    address: "Nakuru" ,
-    image: "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=200", 
-    rating: 4, 
-    review: "Gracie Shoe Hub exceeded my expectations. The shopping process was seamless, and the shoes were absolutely top-notch. Highly recommended!" 
-  },
-  { id: 3, 
-    name: "Sophia Lee", 
-    address: "Kakamega", 
-    image: "https://images.unsplash.com/photo-1701615004837-40d8573b6652?q=80&w=200", 
-    rating: 5, 
-    review: "Amazing support service they have! I always find the best support service when shopping for shoes . Their recommendations never disappoint!" 
-  }
-];
 
-// Facility Icon
-export const facilityIcons = {
-  "Free WiFi": assets.freeWifiIcon,
-  "Free Breakfast": assets.freeBreakfastIcon,
-  "Room Service": assets.roomServiceIcon,
-  "Mountain View": assets.mountainIcon,
-  "Pool Access": assets.poolIcon,
-};
+
 
 export const productBenefits = [
   { 
