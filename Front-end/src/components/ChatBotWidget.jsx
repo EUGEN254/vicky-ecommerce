3ceea4a652d6f3c6452a496c5ccc456607c15ea8
Eugen Bitinyo <bitinyoeugen@gmail.com>
2025-07-23 09:38:40 +0300

    chatbot

--- Front-end/src/components/ChatBotWidget.jsx
+++ Front-end/src/components/ChatBotWidget.jsx
@@ -24,7 +24,7 @@ const ChatBotWidget = ({ show, onClose, onToggle }) => {
     setInput('');
 
     try {
-      const res = await axios.post('http://localhost:3306/api/chatbot/message', { message: input });
+      const res = await axios.post(`${backendUrl}/api/chatbot/message`, { message: input });
       const botMessage = { from: 'bot', text: res.data.reply };
       setMessages((prev) => [...prev, botMessage]);
     } catch {
